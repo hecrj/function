6b5d32eae06a495bac7fe3e97d77311ed136ccad
Héctor Ramón Jiménez <hector@hecrj.dev>
2025-02-18 19:56:26 +0100

    Implement `then` for `Binary`

--- src/binary.rs
+++ src/binary.rs
@@ -22,6 +22,33 @@ pub trait Binary<A, B> {
     fn with(self, a: A) -> impl Fn(B) -> Self::Output + Clone + Send
     where
         A: Clone + Send;
+
+    /// Creates a new function that takes the output of the current function
+    /// and passes it as input to the given one; effectively creating a
+    /// [pipeline] or chain.
+    ///
+    /// [pipeline]: https://en.wikipedia.org/wiki/Pipeline_(software)
+    ///
+    /// ```
+    /// use function::Binary;
+    ///
+    /// fn add(a: u32, b: u32) -> u32 {
+    ///     a + b
+    /// }
+    ///
+    /// fn is_even(n: u32) -> bool {
+    ///     n % 2 == 0
+    /// }
+    ///
+    /// let is_sum_even = add.then(is_even);
+    ///
+    /// assert!(is_sum_even(1, 1));
+    /// assert!(!is_sum_even(3, 4));
+    /// ```
+    fn then<C>(
+        self,
+        f: impl Fn(Self::Output) -> C + Clone + Send,
+    ) -> impl Fn(A, B) -> C + Clone + Send;
 }
 
 impl<F, A, B, O> Binary<A, B> for F
@@ -35,5 +62,12 @@ where
         A: Clone + Send,
     {
         move |b| self(a.clone(), b)
+    }
+
+    fn then<C>(
+        self,
+        f: impl Fn(Self::Output) -> C + Clone + Send,
+    ) -> impl Fn(A, B) -> C + Clone + Send {
+        move |a, b| f(self(a, b))
     }
 }
